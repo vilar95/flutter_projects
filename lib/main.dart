44306b7ece0ddc672eaa66b67548ba381814bdf1
Vilar95 <e.vilar95@gmail.com>
2024-05-03 17:47:21 -0300

    Task List

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,103 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_projects/difficulty.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacity = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 25, 145, 243)),
+        useMaterial3: false,
       ),
       home: Scaffold(
         appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                child: Icon(
+              Icons.list,
+              color: Colors.white,
+            )),
+          ),
           backgroundColor: Colors.blue,
-          title: const Text('Tarefas'),
+          title: const Text(
+            'Tarefas',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
         ),
-        body: ListView(
-          children: [
-            Task('Aprender Flutter'),
-            Task('Hackear o WI-Fi do Vizinho'),
-            Task('MOBIL-628'),
-            Task('Melhorar o Inglês'),
-            Task('Arrumar a Casa'),
-            Task('Pedir Almoço'),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacity ? 1 : 0,
+          duration: Duration(seconds: 1),
+          child: ListView(
+            children: [
+              Task(
+                  'Aprender Flutter',
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                  4),
+              Task(
+                  'Hackear o WI-Fi do Vizinho',
+                  'https://conteudo.imguol.com.br/c/noticias/1f/2022/01/21/hacker-usando-um-laptop-1642786034169_v2_900x506.jpg',
+                  2),
+              Task(
+                  'JIRA - MOBIL-628',
+                  'https://www.iobeya.com/wp-content/uploads/2022/12/Feature-Header-Image-JIRA-1-1024x760.webp',
+                  5),
+              Task(
+                  'Melhorar o Inglês',
+                  'https://culturainglesalondrina.com.br/wp-content/uploads/2022/02/iStock-1051721290.jpg',
+                  4),
+              Task(
+                  'Arrumar a Casa',
+                  'https://www.artmaison.com.br/blog/wp-content/uploads/2020/06/como-limpar-a-casa.jpg',
+                  1),
+              Task(
+                  'Consertar Lanterna',
+                  'https://static.vecteezy.com/ti/vetor-gratis/p1/14448156-chave-reparacao-consertar-ferramentas-mao-modelo-de-logotipo-de-negocios-cor-plana-gratis-vetor.jpg',
+                  1),
+              Task(
+                  'Andar de Jet Ski',
+                  'https://www.arraialferias.com.br/fotos/aluguel-passeio-jet-ski-arraial-do-cabo1629295323.jpg',
+                  3),
+              Task(
+                  'Pescar',
+                  'https://www.alfapesca.com.br/produtos/original/varapesca-84821.png',
+                  4),
+              Task(
+                  'Ler',
+                  'https://cdn-blog.flexi.ink/images/posts/post_22/15873837885e9d8dec3a109_s1000.webp',
+                  3),
+              Task(
+                  'Jogar',
+                  'https://gamedeveloper.com.br/content/images/2016/09/jogue-capa.jpg',
+                  1),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              opacity = !opacity;
+            });
+          },
+          child: opacity
+              ? const Icon(Icons.visibility_off)
+              : const Icon(Icons.visibility),
           backgroundColor: Colors.blue,
         ),
       ),
@@ -39,10 +105,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String name;
-  const Task(this.name, {super.key});
+  final String image;
+  final int difficultyStar;
+  const Task(this.name, this.image, this.difficultyStar, {super.key});
 
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -51,36 +125,106 @@ class Task extends StatelessWidget {
         child: Stack(
           children: [
             Container(
-              color: Colors.blue,
-              height: 140,
-              width: 400,
-            ),
-            Container(
-              color: Colors.white,
-              height: 100,
-              width: 400,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
-                  ),
-                  Container(
-                    width: 200,
-                    child: 
-                    Text(
-                      name,
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Icon(Icons.arrow_drop_up),
-                  ),
-                ],
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.blue,
               ),
+              height: 140,
+            ),
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.white,
+                  ),
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.black26,
+                        ),
+                        width: 80,
+                        height: 100,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.name,
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                          Difficulty(difficultyLevel: widget.difficultyStar, widget: widget)
+                        ],
+                      ),
+                      Container(
+                        height: 74,
+                        width: 74,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                            print(nivel);
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.arrow_drop_up),
+                              Text(
+                                'UP',
+                                style: TextStyle(fontSize: 12),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.orange,
+                          value: (widget.difficultyStar > 0)
+                              ? (nivel / widget.difficultyStar) / 10
+                              : 1,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Nível $nivel',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             )
           ],
         ),
@@ -88,3 +232,5 @@ class Task extends StatelessWidget {
     );
   }
 }
+
+
