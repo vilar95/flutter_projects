9cf067a04154e764d417f5cd904d9840da01e4b4
Vilar95 <e.vilar95@gmail.com>
2024-05-06 17:09:39 -0300

    Forms and navigator

---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({super.key});
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Adicionar Nova Tarefa'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 650,
+              width: 375,
+              decoration: BoxDecoration(
+                color: Colors.blue.shade50,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 3, color: Colors.blue.shade300),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (String? value) {
+                        if (value != null && value.isEmpty) {
+                          return 'Insira o nome da Tarefa';
+                        }
+                        return null;
+                      },
+                      controller: nameController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome da tarefa',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            int.parse(value) > 5 ||
+                            int.parse(value) < 1) {
+                          return 'Insira uma dificuldade entre 1 e 5';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      controller: difficultyController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dificuldade',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      onChanged: (text) {
+                        setState(() {});
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Insira um URL de Imagem!';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.url,
+                      controller: imageController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Imagem',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2, color: Colors.blue),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(imageController.text, errorBuilder:
+                          (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                        return Image.asset('assets/images/nophoto.png');
+                      }, fit: BoxFit.cover),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print(nameController.text);
+                        print(difficultyController.text);
+                        print(imageController.text);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Tarefa salva com sucesso'),
+                          ),
+                        );
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Text('Adicionar'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_projects/components_widget/task.dart';
+import 'package:flutter_projects/screens/form_screen.dart';
 
 class InitialSreen extends StatefulWidget {
   const InitialSreen({super.key});
@@ -9,7 +10,7 @@ class InitialSreen extends StatefulWidget {
 }
 
 class _InitialSreenState extends State<InitialSreen> {
-  bool opacity = true;
+  //bool opacity = true;
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +19,9 @@ class _InitialSreenState extends State<InitialSreen> {
         leading: const Padding(
           padding: EdgeInsets.all(8.0),
           child: Icon(
-                      Icons.list,
-                      color: Colors.white,
-                    ),
+            Icons.list,
+            color: Colors.white,
+          ),
         ),
         backgroundColor: Colors.blue,
         title: const Text(
@@ -30,66 +31,47 @@ class _InitialSreenState extends State<InitialSreen> {
           ),
         ),
       ),
-      body: AnimatedOpacity(
-        opacity: opacity ? 1 : 0,
-        duration: const Duration(seconds: 1),
-        child: ListView(
-          children: const [
-            Task(
-                'Aprender Flutter',
-                'assets/images/Flutter.png',
-                4),
-            Task(
-                'Hackear o WI-Fi do Vizinho',
-                'assets/images/wifi.jpg',
-                2),
-            Task(
-                'JIRA - MOBIL-628',
-                'assets/images/jira.webp',
-                5),
-            Task(
-                'Melhorar o Inglês',
-                'assets/images/ingles.jpg',
-                4),
-            Task(
-                'Arrumar a Casa',
-                'assets/images/casa.jpg',
-                1),
-            Task(
-                'Consertar Lanterna',
-                'assets/images/lanterna.jpg',
-                1),
-            Task(
-                'Andar de Jet Ski',
-                'assets/images/jetski.jpg',
-                3),
-            Task(
-                'Pescar',
-                'assets/images/pescar.png',
-                4),
-            Task(
-                'Ler',
-                'assets/images/ler.webp',
-                3),
-            Task(
-              'Jogar',
-              'assets/images/jogar.jpg',
-              1,
-            ),
-            SizedBox(height: 70,)
-          ],
-        ),
+      body:
+          // AnimatedOpacity(
+          //   opacity: opacity ? 1 : 0,
+          //   duration: const Duration(seconds: 1),
+          //child:
+          ListView(
+        children: const [
+          Task('Aprender Flutter', 'assets/images/Flutter.png', 4),
+          Task('Hackear o WI-Fi do Vizinho', 'assets/images/wifi.jpg', 2),
+          Task('JIRA - MOBIL-628', 'assets/images/jira.webp', 5),
+          Task('Melhorar o Inglês', 'assets/images/ingles.jpg', 4),
+          Task('Arrumar a Casa', 'assets/images/casa.jpg', 1),
+          Task('Consertar Lanterna', 'assets/images/lanterna.jpg', 1),
+          Task('Andar de Jet Ski', 'assets/images/jetski.jpg', 3),
+          Task('Pescar', 'assets/images/pescar.png', 4),
+          Task('Ler', 'assets/images/ler.webp', 3),
+          Task(
+            'Jogar',
+            'assets/images/jogar.jpg',
+            1,
+          ),
+          SizedBox(
+            height: 70,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            opacity = !opacity;
-          });
-        },
         backgroundColor: Colors.blue,
-        child: opacity
-            ? const Icon(Icons.visibility_off)
-            : const Icon(Icons.visibility),
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const FormScreen()));
+          //   setState(() {
+          //     opacity = !opacity;
+          //   });
+          // },
+          // backgroundColor: Colors.blue,
+          // child: opacity
+          //     ? const Icon(Icons.visibility_off)
+          //     : const Icon(Icons.visibility),
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
